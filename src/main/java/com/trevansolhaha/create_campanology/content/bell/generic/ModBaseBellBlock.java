1a14001bf1af23b68698bcc4d88d5298c1d0147f
ChefMooon <139642672+ChefMooon@users.noreply.github.com>
2026-06-22 20:15:53 -0400

    refactor: create AbstractBellBlock, extract common logic

--- src/main/java/com/trevansolhaha/create_campanology/content/bell/generic/ModBaseBellBlock.java
+++ src/main/java/com/trevansolhaha/create_campanology/content/bell/generic/ModBaseBellBlock.java
@@ -11,7 +11,6 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
@@ -22,10 +21,9 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.Nullable;
 
-public class ModBaseBellBlock extends Block implements IWrenchable, EntityBlock {
+public class ModBaseBellBlock extends Block implements AbstractBellBlock, IWrenchable, EntityBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final EnumProperty<ModBellSizes> SIZE = EnumProperty.create("size", ModBellSizes.class);
     public ModBaseBellBlock(Properties properties) {
@@ -106,26 +104,6 @@ public class ModBaseBellBlock extends Block implements IWrenchable, EntityBlock 
     @Override
     public RenderShape getRenderShape(BlockState state) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
-    }
-
-    protected Direction getExplosionSourceDirection(Explosion explosion, BlockPos blockPos, Direction facing) {
-        Vec3 explosionCenter = explosion.center();
-        Vec3 blockCenter = Vec3.atCenterOf(blockPos);
-        double dotProduct = getDotProduct(facing, explosionCenter, blockCenter);
-
-        if (dotProduct >= 0) {
-            return facing;
-        } else {
-            return facing.getOpposite();
-        }
-    }
-
-    private static double getDotProduct(Direction facing, Vec3 explosionCenter, Vec3 blockCenter) {
-        double dx = explosionCenter.x - blockCenter.x;
-        double dz = explosionCenter.z - blockCenter.z;
-        int stepX = facing.getStepX();
-        int stepZ = facing.getStepZ();
-        return (dx * stepX) + (dz * stepZ);
     }
 
     protected void playBellSound(Level level, BlockPos pos, ModBellSizes size, SoundEvent soundEvent) {
